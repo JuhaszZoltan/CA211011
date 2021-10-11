cffb1d695c6c998e3ac89974cb8302397588896e
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2021-10-11 09:37:18 +0200

    Add project files.

---
+++ CA211011/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA211011
+{
+    class Program
+    {
+        static void Main()
+        {
+            string connectionString = 
+                @"Server=(localdb)\MSSQLLocalDB;" +
+                "Database=teszt;";
+
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            var sqlCommand = new SqlCommand("SELECT * FROM emberek;", connection);
+            var sqlDataReader = sqlCommand.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                Console.WriteLine(
+                    $"{sqlDataReader.GetInt32(0) * 10} " +
+                    $"{sqlDataReader["nev"]} " +
+                    $"{sqlDataReader[2]}");
+            }
+
+            sqlDataReader.Close();
+
+            //-----------------
+            Console.Write("új név: ");
+            string ujNev = Console.ReadLine();
+            Console.Write("új telefonszám: ");
+            string ujTelefonszam = Console.ReadLine();
+
+            sqlCommand = new SqlCommand(
+                $"INSERT INTO emberek " +
+                $"VALUES ('{ujNev}', '{ujTelefonszam}');",
+                connection);
+
+            var sqlDataAdapter = new SqlDataAdapter();
+            sqlDataAdapter.InsertCommand = sqlCommand;
+            sqlDataAdapter.InsertCommand.ExecuteNonQuery();
+
+            Console.WriteLine("megcsinmáltam ge!");
+
+            connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
